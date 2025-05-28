bbfb55845b2a0d6cf8ae6fa43ae41486ce1a3185
sofii7myloo <luagusu@gmail.com>
2025-05-28 15:43:25 +0300

    Написали первую работу

---
+++ index.js
@@ -0,0 +1,11 @@
+console.log('Привет мир');
+
+let x = +prompt('Введите x');
+let y = 10;
+
+let t;
+
+t = (Math.cos(Math.PI / 7) * Math.pow(Math.sin(x - 8*y), 2)) / 2.7*(x-Math.PI);
+
+console.log(t);
+console.log(x);
