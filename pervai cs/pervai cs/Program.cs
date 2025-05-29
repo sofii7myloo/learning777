5fd38d9366bfb948011436af79b0e6e27c2b8cc3
sofii7myloo <luagusu@gmail.com>
2025-05-29 14:09:39 +0300

    работа с#

---
+++ pervai cs/pervai cs/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace pervai_cs { 
+class Program {
+        static void Main(string[] args) {
+            //Console.WriteLine("fgdg");
+            double x= Convert.ToDouble(Console.ReadLine());
+            double y= Convert.ToDouble(Console.ReadLine());
+            double t;
+            
+            t = Math.Cos(3.14 / 7) * (Math.Pow(Math.Sin(x - 8 * y), 2) / (2.7 * (x - 3.14)));
+            Console.WriteLine(t);
+
+        }
+   }
+}
+
