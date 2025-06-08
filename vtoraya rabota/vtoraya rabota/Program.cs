bf0b7bc5158525accd2ec77d73ef5bcd012d0297
sofii7myloo <luagusu@gmail.com>
2025-06-08 15:26:24 +0300

    выполнили 1 пример 2 работы на С#

--- vtoraya rabota/vtoraya rabota/Program.cs
+++ vtoraya rabota/vtoraya rabota/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using static System.Math;
+
 
 namespace vtoraya_rabota
 {
@@ -10,15 +8,35 @@ namespace vtoraya_rabota
     {
         static void Main(string[] args)
         {
-            double x=4;
-            double y=3;
-            double e;
-            double c=7;
-            double d=1;
-            if (x*y > 3)
+            Console.WriteLine("Введите x:");
+            double x = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите y:");
+            double y = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите c:");
+            double c = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите d:");
+            double d = double.Parse(Console.ReadLine());
+
+            double e = 0;
+
+
+            if (x * y > 3) 
             {
-                double maxEl = Math.Log
+                e = Math.Max(Math.Pow(x, 3), Math.Pow(Math.Log10(x * y), c * d));
             }
+            else if (0 <= x * y && x * y <= 3)
+            { 
+                e = 3 * Math.Min(x, Math.Min(y, Math.Max(c * x, d * y)));
+            }
+            else if (x * y < 0)
+            { 
+                e = Math.Pow(2, c * d) - x;
+            }
+
+            Console.WriteLine($"Результат: e= {e}");
         }
     }
 }
--- vtoraya rabota/vtoraya rabota/obj/Debug/net8.0/vtoraya rabota.AssemblyInfo.cs
+++ vtoraya rabota/vtoraya rabota/obj/Debug/net8.0/vtoraya rabota.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("vtoraya rabota")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fd38d9366bfb948011436af79b0e6e27c2b8cc3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fa7ef06ff783fe04631c86aa64aef3ff28664dcc")]
 [assembly: System.Reflection.AssemblyProductAttribute("vtoraya rabota")]
 [assembly: System.Reflection.AssemblyTitleAttribute("vtoraya rabota")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
